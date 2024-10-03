e6a6e7a14b5668d8325ddeb0c7c98d0223dc858e
namuuCY <wkdcjfdud13@gmail.com>
2024-10-04 02:33:49 +0900

    Feat : 서로 다른 박스 간 이동 구현 - usecontext, enum 사용.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
-import { useState, useContext, createContext } from 'react'
+import { useState, createContext } from 'react'
 import '@/App.css'
 import BasicBox from '@/components/BasicBox'
 
-const TotalContext = createContext({})
+const defaultObject = {
+  0: [],
+  1: [],
+  2: [],
+}
+const TotalContext = createContext({ lists: defaultObject, setLists: (state) => {} })
 
 function App() {
   const [lists, setLists] = useState({
@@ -13,12 +18,12 @@ function App() {
   return (
     <TotalContext.Provider value={{ lists, setLists }}>
       <div className='flex'>
-        <BasicBox title={'To Do'} />
-        <BasicBox title={'In Progress'} />
-        <BasicBox title={'Done'} />
+        <BasicBox title={'To Do'} listIndex={0} />
+        <BasicBox title={'In Progress'} listIndex={1} />
+        <BasicBox title={'Done'} listIndex={2} />
       </div>
     </TotalContext.Provider>
   )
 }
 
-export default App
+export { App, TotalContext }
--- src/components/BasicBox.jsx
+++ src/components/BasicBox.jsx
@@ -1,5 +1,6 @@
-import { useRef, useState } from 'react'
-import { produce } from 'immer'
+import { useRef, useState, useContext } from 'react'
+import { TotalContext } from '@/App'
+import { current, produce } from 'immer'
 
 import {
   Card,
@@ -11,26 +12,54 @@ import {
 } from '@/components/ui/card'
 import ChangeToInput from '@/components/ChangeToInput'
 
-export default function BasicBox({ title }) {
+export default function BasicBox({ title, listIndex }) {
   const inputRef = useRef()
+  const { lists, setLists } = useContext(TotalContext)
   const [item, setItem] = useState([])
 
   function addTodo() {
-    console.log(inputRef.current.value)
+    const changed = produce(lists, (draft) => {
+      draft[listIndex] = [...draft[listIndex], inputRef.current.value]
+    })
+    setLists(changed)
+    console.log(lists)
     setItem((prev) => [...prev, inputRef.current.value])
   }
-  const handleDragStart = (e, index) => {
+
+  const handleDragStart = (e, index, prevListIndex) => {
+    const prev = { prevListIndex, prevItemIndex: index }
+    e.dataTransfer.clearData()
     e.dataTransfer.setData('text/plain', index)
+    e.dataTransfer.setData('object', JSON.stringify(prev))
   }
-  const handleDrop = (e, dropIndex) => {
+
+  const handleDrop = (e, dropIndex, nextListIndex) => {
     e.preventDefault()
+    console.log(nextListIndex)
     const draggedIndex = e.dataTransfer.getData('text')
+    const prev = JSON.parse(e.dataTransfer.getData('object'))
 
     const currentItems = [...item]
 
     const draggedItem = currentItems.splice(draggedIndex, 1)
     currentItems.splice(dropIndex, 0, draggedItem)
     setItem(currentItems)
+
+    // const draggedList = produce(lists, (draft) => {
+    //   const temp = draft[prev.prevListIndex][prev.prevItemIndex]
+    //   draft[prev.prevListIndex] = draft[prev.prevListIndex].splice(prev.prevItemIndex, 1)
+    //   draft[nextListIndex] = draft[nextListIndex].splice(dropIndex, 0, temp)
+    // })
+    const draggedList = produce(lists, (draft) => {
+      // 이전 리스트에서 아이템을 제거
+      const [draggedItem] = draft[prev.prevListIndex].splice(prev.prevItemIndex, 1)
+
+      // 새로운 리스트에 아이템 삽입
+      draft[nextListIndex].splice(dropIndex, 0, draggedItem)
+    })
+
+    setLists(draggedList)
+    console.log(lists)
   }
   const handleDragOver = (e) => {
     e.preventDefault()
@@ -46,14 +75,14 @@ export default function BasicBox({ title }) {
         <Card className='flex-col items-center justify-center'>
           <CardDescription className=' items-center justify-center'>
             <ChangeToInput event={addTodo} reference={inputRef} />
-            {item?.map((each, index) => {
+            {lists[listIndex].map((each, index) => {
               return (
                 <>
                   <Card
                     draggable
-                    onDragStart={(e) => handleDragStart(e, index)}
+                    onDragStart={(e) => handleDragStart(e, index, listIndex)}
                     onDragOver={handleDragOver}
-                    onDrop={(e) => handleDrop(e, index)}
+                    onDrop={(e) => handleDrop(e, index, listIndex)}
                     className='m-2 h-10 w-4/5 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium'
                     key={index}
                   >
