eb0ff77fc3ba47404b03dea7cb6e5560ce643912
namuuCY <wkdcjfdud13@gmail.com>
2024-10-04 00:59:15 +0900

    Feat : 1개 단위 리스트에 대한 이동기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
-import { useState } from 'react'
+import { useState, useContext, createContext } from 'react'
 import '@/App.css'
 import BasicBox from '@/components/BasicBox'
+
+const TotalContext = createContext({})
+
 function App() {
+  const [lists, setLists] = useState({
+    0: [],
+    1: [],
+    2: [],
+  })
   return (
-    <>
-      <BasicBox />
-    </>
+    <TotalContext.Provider value={{ lists, setLists }}>
+      <div className='flex'>
+        <BasicBox title={'To Do'} />
+        <BasicBox title={'In Progress'} />
+        <BasicBox title={'Done'} />
+      </div>
+    </TotalContext.Provider>
   )
 }
 
--- src/components/BasicBox.jsx
+++ src/components/BasicBox.jsx
@@ -1,6 +1,6 @@
 import { useRef, useState } from 'react'
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
+import { produce } from 'immer'
+
 import {
   Card,
   CardContent,
@@ -11,36 +11,59 @@ import {
 } from '@/components/ui/card'
 import ChangeToInput from '@/components/ChangeToInput'
 
-export default function BasicBox() {
+export default function BasicBox({ title }) {
   const inputRef = useRef()
-  const [content, setContent] = useState([])
+  const [item, setItem] = useState([])
 
   function addTodo() {
     console.log(inputRef.current.value)
-    setContent((prev) => [...prev, inputRef.current.value])
+    setItem((prev) => [...prev, inputRef.current.value])
+  }
+  const handleDragStart = (e, index) => {
+    e.dataTransfer.setData('text/plain', index)
+  }
+  const handleDrop = (e, dropIndex) => {
+    e.preventDefault()
+    const draggedIndex = e.dataTransfer.getData('text')
+
+    const currentItems = [...item]
+
+    const draggedItem = currentItems.splice(draggedIndex, 1)
+    currentItems.splice(dropIndex, 0, draggedItem)
+    setItem(currentItems)
+  }
+  const handleDragOver = (e) => {
+    e.preventDefault()
   }
 
   return (
     <>
       <Card className='flex-col min-w-96 min-h-96'>
         <CardHeader>
-          <CardTitle>To Do</CardTitle>
+          <CardTitle>{title}</CardTitle>
         </CardHeader>
-        <CardDescription>
-          <ChangeToInput event={addTodo} reference={inputRef} />
-          {content?.map((each, index) => {
-            return (
-              <>
-                <Card
-                  className='h-10 w-4/5 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium'
-                  key={index}
-                >
-                  {each}
-                </Card>
-              </>
-            )
-          })}
-        </CardDescription>
+
+        <Card className='flex-col items-center justify-center'>
+          <CardDescription className=' items-center justify-center'>
+            <ChangeToInput event={addTodo} reference={inputRef} />
+            {item?.map((each, index) => {
+              return (
+                <>
+                  <Card
+                    draggable
+                    onDragStart={(e) => handleDragStart(e, index)}
+                    onDragOver={handleDragOver}
+                    onDrop={(e) => handleDrop(e, index)}
+                    className='m-2 h-10 w-4/5 items-center justify-center whitespace-nowrap rounded-md text-sm font-medium'
+                    key={index}
+                  >
+                    {each}
+                  </Card>
+                </>
+              )
+            })}
+          </CardDescription>
+        </Card>
       </Card>
     </>
   )
